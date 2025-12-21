d86457467e84cf7ec93b84aa26c2e78a3a3c670f
Lilen <lilen.chu@hotmail.com>
2025-12-21 12:37:21 -0300

    Ejercicio 4 terminado

--- actividades.js
+++ actividades.js
@@ -51,12 +51,14 @@ Solicita al usuario la cantidad de productos comprados y el precio unitario. Usa
 const calcularTotal = (cantidad, precio) => {
     let descuento = 0;
     if (cantidad >= 0 && cantidad < 5) {
-        ;
+        descuento = 0;
     } else if (cantidad >= 5 && cantidad < 10) {
-        ;
+        descuento = 0.10;
     } else {
-        ;
+        descuento = 0.20;
     }
+    let total = cantidad * precio;
+    return total - (total * descuento);
 };
 
 let cantidad = parseInt(prompt("Ingrese la cantidad de productos: "));
